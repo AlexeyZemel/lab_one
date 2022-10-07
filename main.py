b2929694b1387c2f1d0eda0ed98b14a431049beb
Alexey Zemel <loyxa.zemel@gmail.com>
2022-10-07 13:59:08 +0400

    Добавлен код создания папок и проверок их на существование

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-#test
+import os
+from bs4 import BeautifulSoup
+import requests
+import re
+from re import sub
+from decimal import Decimal
+import io
+from datetime import datetime
+import pandas as pd
+import cv2
+import urllib.request
+import random
+
+if not os.path.isdir('E:\dataset'):
+    os.mkdir('E:\dataset')
+
+if not os.path.isdir('E:\dataset\\brownbears'):
+    os.mkdir('E:\dataset\\brownbears')
+
+if not os.path.isdir('E:\dataset\polarbears'):
+    os.mkdir('E:\dataset\polarbears')
