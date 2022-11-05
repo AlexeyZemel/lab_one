1966418e61ec07cdaeaa664500cc34333299e85a
Alexey Zemel <loyxa.zemel@gmail.com>
2022-11-05 19:14:53 +0400

    Добавлен код копирования данных второго класса в другую директорию

--- second_task.py
+++ second_task.py
@@ -30,6 +30,17 @@ def copy_to_another(path_to_another: str, path_to_dataset: str) -> None:
             if os.path.isfile(path):
                 shutil.copyfile(path, new_path)
                 write_copy(i, class_name, new_path)
+    
+    class_name = 'brownbears'
+    path_to_pbear = path_to_dataset + '/' + class_name
+    sum_files = len([fl for fl in os.listdir(path_to_pbear) if os.path.isfile(os.path.join(path_to_pbear, fl))])
+
+    for i in tqdm(range(0, sum_files), colour= 'green'):
+            path = path_to_pbear + f'/{str(i).zfill(4)}.jpg'
+            new_path = path_to_another + f'/{class_name}_{str(i).zfill(4)}.jpg'
+            if os.path.isfile(path):
+                shutil.copyfile(path, new_path)
+                write_copy(i, class_name, new_path)
 
 path_to_dataset = 'E:/dataset'
 path_to_another_dataset = 'E:/dataset/another_dataset'
