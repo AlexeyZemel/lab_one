73ea86e1404dd82e347012e24f207479f4a627e6
Alexey Zemel <loyxa.zemel@gmail.com>
2022-11-05 17:54:30 +0400

    Добавлен код записи аннотаций для второго класса. Конечный вид скрипта

--- first_task.py
+++ first_task.py
@@ -21,6 +21,18 @@ def write_annotation (dataset_path: str, csv_path: str) -> None:
                                         'Relative way': f'dataset/{class_name}/{str(i).zfill(4)}.jpg',
                                         'Class': class_name})                                    
 
+    class_name = 'brownbears'
+    path_to_pbear = dataset_path + '/' + class_name
+    sum_files = len([fl for fl in os.listdir(path_to_pbear) if os.path.isfile(os.path.join(path_to_pbear, fl))])
+
+    for i in tqdm(range(0, sum_files), colour='green'):
+        with open(csv_path, 'a', newline='', encoding='utf-8') as f:
+            write_in_file = csv.DictWriter(f, fieldnames = headings, delimiter=';', quoting=csv.QUOTE_ALL)
+            path = f'E:/dataset/{class_name}/{str(i).zfill(4)}.jpg'
+            if os.path.isfile(path):
+                write_in_file.writerow({'Absolute way': dataset_path + f'/{class_name}/{str(i).zfill(4)}.jpg',
+                                        'Relative way': f'dataset/{class_name}/{str(i).zfill(4)}.jpg',
+                                        'Class': class_name})
 
 path_to_dataset = 'E:\dataset'
 csv_path = 'dataset.csv'
