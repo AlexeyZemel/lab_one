d014681b021d6254caf8f52cad5ba7e36f7608ee
Alexey Zemel <loyxa.zemel@gmail.com>
2022-11-05 16:56:30 +0400

    Добавлен код записи заголовков

--- first_task.py
+++ first_task.py
@@ -1,3 +1,13 @@
 import csv
 import os
 
+def write_annotation (dataset_path: str, csv_path: str) -> None:
+    headings = ['Absolute way', 'Relative way', 'Class']
+    with open(csv_path, 'w', newline='', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, fieldnames = headings, delimiter=';', quoting=csv.QUOTE_ALL)
+        writer.writeheader()
+
+
+path_to_dataset = 'E:\dataset'
+csv_path = 'dataset.csv'
+write_annotation(path_to_dataset, csv_path)
