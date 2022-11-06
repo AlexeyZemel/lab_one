5a21af1fbece849429902116524b9f53bb9feb1b
Alexey Zemel <loyxa.zemel@gmail.com>
2022-11-06 19:55:35 +0400

    Написаны dosctring к методам класса

--- fifth_task.py
+++ fifth_task.py
@@ -3,6 +3,15 @@ import os
 
 class Iterator:
     def __init__(self, class_name: str, path: str) -> None:
+        
+        '''
+            Инициализирует объект класса итератор
+
+            Ключевые аргументы:
+                class_name(str): имя класса
+                path(str): путь до csv файла
+        '''
+
         self.class_name = class_name
         self.path = path
         self.counter = 0
@@ -22,8 +31,19 @@ class Iterator:
         return self
 
     def __next__(self) -> None:
+        
+        '''
+            Функция переключения на следующий экземпляр в списке, возвращает
+            путь до этого экземпляра
+        '''
+
         if self.counter < len(self.list):
             self.counter +=1
             return self.list[self.counter][0]
         elif self.counter == len(self.list):
             raise StopIteration
+
+if __name__ == '__main__':
+    iter = Iterator ('polarbears', 'dataset.csv')
+    for j in iter:
+        print (j)
