9c6a539f4c1cf5d02daf90b6ad2ff219285eb325
Alexey Zemel <loyxa.zemel@gmail.com>
2022-11-06 12:46:07 +0400

    Созданы файлы для третьего скрипта

---
+++ third_task.py
@@ -0,0 +1,9 @@
+import os
+import csv
+import random
+from tqdm import tqdm
+
+if __name__ == '__main__':
+    path_to_dataset = 'E:/dataset'
+    path_to_random = 'E:/dataset/random_dataset'
+    csv_path = 'random_dataset.csv'
