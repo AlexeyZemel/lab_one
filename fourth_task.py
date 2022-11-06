9f5fb4e29ed5df7431a6e9b40a62c4811d361096
Alexey Zemel <loyxa.zemel@gmail.com>
2022-11-06 20:35:38 +0400

    Исправлен скрипт и добавлен код запуска функции

--- fourth_task.py
+++ fourth_task.py
@@ -6,23 +6,25 @@ def get_item(class_name: str, csv_path: str, count: int) -> str or None:
     '''
         Функция возвращает путь элемента следующего за count, если элемента нет
         возвращает None
-
         Ключевые аргументы:
             class_name(str): метка класса
             csv_path(str): путь csv файла
             count(int): номер элемента
     '''
-    
-    path_to_class = 'E:/dataset/' + class_name
-    sum_files = len([fl for fl in os.listdir(path_to_class) if os.path.isfile(os.path.join(path_to_class, fl))])
-    if count + 1 < sum_files:
-        rows = []
-        headings = ['Absolute way', 'Relative way', 'Class']
-        with open(csv_path, 'r', encoding = 'utf8') as file:
-                read = csv.DictReader(file, fieldnames = headings, delimiter = ';')
-                for row in read:
-                    if row['Class'] == class_name:
-                        rows.append([row['Absolute way'], row['Relative way'], row['Class']])
+    rows = []
+    headings = ['Absolute way', 'Relative way', 'Class']
+    with open(csv_path, 'r', encoding = 'utf8') as file:
+            read = csv.DictReader(file, fieldnames = headings, delimiter = ';')
+            for row in read:
+                if row['Class'] == class_name:
+                    rows.append([row['Absolute way'], row['Relative way'], row['Class']])
+    if count + 1 < len(rows):
         row_dict = {headings[k]:rows[count + 1][k] for k in range(0, len(headings))}
         return row_dict['Absolute way']
     else: return None
+
+if __name__ == '__main__':
+    for i in range(0,1050):
+        item = get_item('brownbears', 'another_dataset.csv', i)
+        print(item)
+        
