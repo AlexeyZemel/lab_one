9423882cc771ed96a194cfb34fe4b8d4f4cd20f3
Alexey Zemel <loyxa.zemel@gmail.com>
2022-11-06 13:07:55 +0400

    Добавлен код копирования данных одного класса с рандомными номерами

--- third_task.py
+++ third_task.py
@@ -1,9 +1,36 @@
 import os
 import csv
 import random
+import shutil
 from tqdm import tqdm
+
+def copy_to_random(dataset_path: str, random_path: str, csv_path: str) -> None:
+    if not os.path.exists(random_path):
+        os.mkdir(random_path)
+    
+    headings = ['Absolute way', 'Relative way', 'Class']
+    with open('another_dataset.csv', 'w', newline='', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, fieldnames = headings, delimiter=';', quoting=csv.QUOTE_ALL)
+        writer.writeheader() 
+    
+    random_numbers = list(range(0,10001))
+    random.shuffle(random_numbers)
+    random_num1 = random_numbers[:len(random_numbers)//2]
+    random_num2 = random_numbers[len(random_numbers)//2:]
+
+    class_name = 'polarbears'
+    path_to_pbear = dataset_path + '/' + class_name
+    sum_files = len([fl for fl in os.listdir(path_to_pbear) if os.path.isfile(os.path.join(path_to_pbear, fl))])
+
+    for i in tqdm(range(0, sum_files), colour= 'green'):
+            path = path_to_pbear + f'/{str(i).zfill(4)}.jpg'
+            new_path = random_path + f'{str(random_num1[i]).zfill(5)}.jpg'
+            if os.path.isfile(path):
+                shutil.copyfile(path, new_path)
+
 
 if __name__ == '__main__':
     path_to_dataset = 'E:/dataset'
     path_to_random = 'E:/dataset/random_dataset'
     csv_path = 'random_dataset.csv'
+    copy_to_random(path_to_dataset, path_to_random, csv_path)
