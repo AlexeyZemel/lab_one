edd104d3182d57325ad295d7e93fe331d97d2c87
Alexey Zemel <loyxa.zemel@gmail.com>
2022-11-19 16:41:31 +0400

    Сделаны необходимые импорты

---
+++ main_window.py
@@ -0,0 +1,11 @@
+import os
+import sys
+from _iterator import Iterator
+from _copy_rand import copy_to_random
+from _copy import copy_to_another
+from _writer_anno import write_annotation
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+class MainWindow(QWidget):
