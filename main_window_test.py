c05d1fe040b807f46c138aa8cf88ec7e58c0a07f
Alexey Zemel <loyxa.zemel@gmail.com>
2022-11-22 21:51:46 +0400

    Внесены конечные правки

--- main_window_test.py
+++ main_window_test.py
@@ -4,12 +4,12 @@ from _iterator import Iterator
 from _copy_rand import copy_to_random
 from _copy import copy_to_another
 from _writer_anno import write_annotation
-from PyQt5 import QtWidgets
-from PyQt5.QtGui import QPixmap
+from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
-class MainWindow(QMainWindow):
+
+class MainWindow(QWidget):
     def __init__(self) -> None:
         """
         Инициализирует объект класса
@@ -22,45 +22,53 @@ class MainWindow(QMainWindow):
         """
         Создание GUI
         """
-        self.setGeometry(500, 500, 900, 420)
+        QToolTip.setFont(QFont("SansSerif", 7))
+
+        self.setGeometry(500, 500, 500, 420)
         self.setWindowTitle("Bears")
         self.setWindowIcon(QIcon("icon.jpg"))
 
-        pix = QPixmap('icon.jpg')
-        item = QGraphicsPixmapItem(pix)
-        scene = QGraphicsScene(self)
-        scene.addItem(item)
-        view = QGraphicsView(scene)
-        self.setCentralWidget(view)
+        btn_first = QPushButton("Create annotation from dataset", self)
+        btn_first.setToolTip("<i>Create</i>")
+        btn_first.resize(btn_first.sizeHint())
+        btn_first.move(50, 50)
+        btn_first.clicked.connect(self.on_click_dataset)
 
-        menubar = self.menuBar()
-        first = menubar.addMenu("Create annotation dataset")
-        action1 = QAction(self)
-        action1.triggered.connect(self.on_click_dataset)
-        first.addAction(action1)
+        btn_second = QPushButton(
+            "Copying to another directory and create annotation", self
+        )
+        btn_second.setToolTip("<i>Copy</i>")
+        btn_second.resize(btn_second.sizeHint())
+        btn_second.move(50, 100)
+        btn_second.clicked.connect(self.on_click_another)
 
-        second = menubar.addMenu("Copying to another directory")
-        action2 = QAction(self)
-        action2.triggered.connect(self.on_click_another)
-        second.addAction(action2)
+        btn_third = QPushButton(
+            "Copying to new directory with random number and create annotation", self
+        )
+        btn_third.setToolTip("<i>Copy with random number</i>")
+        btn_third.resize(btn_third.sizeHint())
+        btn_third.move(50, 150)
+        btn_third.clicked.connect(self.on_click_random)
 
-        third = menubar.addMenu("Copying to new directory with random number")
-        action3 = QAction(self)
-        action3.triggered.connect(self.on_click_random)
-        third.addAction(action3)
+        btn_pbear = QPushButton("Polar bear", self)
+        btn_pbear.setToolTip("<i>Shows the polar bear</i>")
+        btn_pbear.resize(btn_pbear.sizeHint())
+        btn_pbear.move(50, 200)
+        btn_pbear.clicked.connect(self.on_click_next_pbear)
 
-        fourth = menubar.addMenu("Polar bear")
-        action4 = QAction(self)
-        action4.triggered.connect(self.on_click_next_pbear)
-        fourth.addAction(action4)
-
-        fifth = menubar.addMenu("Brown bear")
-        action5 = QAction(self)
-        action5.triggered.connect(self.on_click_next_bbear)
-        fifth.addAction(action5)
-
+        btn_bbear = QPushButton("Brown bear", self)
+        btn_bbear.setToolTip("<i>Shows the brown bear</i>")
+        btn_bbear.resize(btn_bbear.sizeHint())
+        btn_bbear.move(50, 250)
+        btn_bbear.clicked.connect(self.on_click_next_bbear)
+        """
+        self.lbl = QtWidgets.QLabel(self)
+        self.pix = QtGui.QPixmap('icon.jpg')
+        self.lbl.setPixmap(self.pix)
+        self.lbl.resize(500, 500)
+        self.lbl.move(200, 200)
+        """
         self.show()
-
 
     def on_click_dataset(self) -> None:
         """
@@ -126,12 +134,8 @@ class MainWindow(QMainWindow):
 
                 if self.dialog.clickedButton().text() == "Next bear":
                     image = iter.__next__()
-                    pix = QPixmap(image)
-                    item = QGraphicsPixmapItem(pix)
-                    scene = QGraphicsScene(self)
-                    scene.addItem(item)
-                    view = QGraphicsView(scene)
-                    self.setCentralWidget(view)
+                    self.image_window = ShowImage(image, self)
+                    self.image_window.show()
 
                 if self.dialog.clickedButton().text() == "Stop":
                     break
@@ -158,15 +162,32 @@ class MainWindow(QMainWindow):
 
                 if self.dialog.clickedButton().text() == "Next bear":
                     image = iter.__next__()
-                    pix = QPixmap(image)
-                    item = QGraphicsPixmapItem(pix)
-                    scene = QGraphicsScene(self)
-                    scene.addItem(item)
-                    view = QGraphicsView(scene)
-                    self.setCentralWidget(view)
+                    self.image_window = ShowImage(image, self)
+                    self.image_window.show()
 
                 if self.dialog.clickedButton().text() == "Stop":
                     break
+
+
+class ShowImage(QtWidgets.QWidget):
+    def __init__(self, path, parent=None) -> None:
+        """
+        Конструктор класса
+        """
+        super().__init__(parent, QtCore.Qt.Window)
+        self.path_image = path
+        self.viev()
+
+    def viev(self) -> None:
+        """
+        Создаёт окно с картинкой
+        """
+        label = QLabel(self)
+        pixmap = QPixmap(self.path_image)
+        label.setPixmap(pixmap)
+        self.resize(pixmap.width(), pixmap.height())
+        self.move(600, 500)
+        self.setWindowTitle("Bear")
 
 
 if __name__ == "__main__":
@@ -174,4 +195,3 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = MainWindow()
     sys.exit(app.exec_())
-    
